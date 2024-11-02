4c4afd1c03e67ff79e30ac079ca609716ce68da4
Avanthika <83574562+Avanthu123@users.noreply.github.com>
2024-11-02 22:51:52 +0530

    heh

--- others/my.js
+++
@@ -1,6 +0,0 @@
-// Add a click event to each review button
-document.querySelectorAll(".review-btn").forEach(button => {
-    button.addEventListener("click", () => {
-        alert("Redirecting to review page...");
-    });
-});
